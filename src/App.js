daa3ebd62fe5bfb1a20fc6c2b7a041f72aa39ebe
gon <gonzalomartinezgonzalez@gmail.com>
2023-08-03 18:30:24 +0200

    working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,92 @@
-import './App.css';
-import { DrumPad } from './components/DrumPad';
+import "./App.css";
+import { DrumPad } from "./components/DrumPad";
+
 // import and name audio tracks here
-import doh from "./audios/doh1.mp3"
+import belching from "./audios/belching.mp3";
+import boring from "./audios/boring.mp3";
+import champion from "./audios/champion.mp3";
+import crap from "./audios/crap.mp3";
+import dancewgay from "./audios/dancewgay.mp3";
+import doh from "./audios/doh1.mp3";
+import haha from "./audios/haha.mp3";
+import mchocola from "./audios/mchocola.mp3";
+import thksnthn from "./audios/thksnthn.mp3";
+import { useState } from "react";
 
 function App() {
+  const [displayText, setDisplayText] = useState("");
+
+  // f to recover text from child component DrumPad to display audio title on display
+  const recoverDisplayText = (text) => {
+    setDisplayText(text);
+  };
+
   return (
-    <article id='drum-machine'>
-      <DrumPad assignedKey="Q" audioTitle="Doh" audioFile={doh}></DrumPad>
-    </article>
+    <>
+      <header>Drum machine</header>
+      <main id="drum-machine">
+        <div id="padKeyboard">
+        <DrumPad
+          assignedKey="Q"
+          audioTitle="Belching"
+          audioFile={belching}
+          recoverDisplayText={recoverDisplayText}
+        ></DrumPad>
+        <DrumPad
+          assignedKey="W"
+          audioTitle="Doh"
+          audioFile={doh}
+          recoverDisplayText={recoverDisplayText}
+        ></DrumPad>
+        <DrumPad
+          assignedKey="E"
+          audioTitle="Crap"
+          audioFile={crap}
+          recoverDisplayText={recoverDisplayText}
+        ></DrumPad>
+        <DrumPad
+          assignedKey="A"
+          audioTitle="Boring"
+          audioFile={boring}
+          recoverDisplayText={recoverDisplayText}
+        ></DrumPad>
+        <DrumPad
+          assignedKey="S"
+          audioTitle="Haha"
+          audioFile={haha}
+          recoverDisplayText={recoverDisplayText}
+        ></DrumPad>
+        <DrumPad
+          assignedKey="D"
+          audioTitle="Chocolate"
+          audioFile={mchocola}
+          recoverDisplayText={recoverDisplayText}
+        ></DrumPad>
+        <DrumPad
+          assignedKey="Z"
+          audioTitle="Champion"
+          audioFile={champion}
+          recoverDisplayText={recoverDisplayText}
+        ></DrumPad>
+        <DrumPad
+          assignedKey="X"
+          audioTitle="I danced with a gay!"
+          audioFile={dancewgay}
+          recoverDisplayText={recoverDisplayText}
+        ></DrumPad>
+        <DrumPad
+          assignedKey="C"
+          audioTitle="Dear God, thanks for nothing"
+          audioFile={thksnthn}
+          recoverDisplayText={recoverDisplayText}
+        ></DrumPad>
+        </div>
+        <article id="display">{displayText}</article>
+        <article id="controls">
+        </article>
+      </main>
+      <footer>Gonzalo M.G.</footer>
+    </>
   );
 }
 
--- src/components/DrumPad/index.js
+++ src/components/DrumPad/index.js
@@ -1,30 +1,65 @@
 /* 
 button to play sound
 */
+import { useState } from "react";
 import "./index.css";
 import PropTypes from "prop-types";
 
-export const DrumPad = ({ assignedKey, audioTitle, audioFile }) => {
-   
-  //create audio instance  
-  let audio = new Audio(audioFile);
+export const DrumPad = ({
+  assignedKey,
+  audioTitle,
+  audioFile,
+  recoverDisplayText,
+}) => {
+  // create audio instance
+  const audio = new Audio(audioFile);
+  // states to activate and modify css when playing
+  const [playing, setPlaying] = useState("notPlaying");
 
   // f to play audio
-  const play = () => {
+  const play = (event) => {
+    console.log("play - called")
     audio.play();
-  };
+    // send text to display
+    console.log("play - recoverDisplayText")
+    recoverDisplayText(audioTitle);
+    // modify css
+    console.log("play - setPlaying")
+    setPlaying("isPlaying");
+    setTimeout(() => {
+      console.log("play - setPlaying")
+      setPlaying("notPlaying");
+    }, 1000);
     
+    // stop event to prevent play() from being called multiple times per keypress; onClick does not have .stopImmediatePropagation() as function
+    if (event.type === "keydown") {
+      event.stopImmediatePropagation();
+    }
+  };
+
   // f to handle keypress by user: play audio if pressed key corresponds to assigned key
   const handleKeyPress = (event) => {
-   if (event.key === (assignedKey.toLowerCase() || assignedKey.toUpperCase())) {
-    play();
-   }
-  }
+    if (
+      event.key === assignedKey.toLowerCase() ||
+      event.key === assignedKey.toUpperCase()
+    ) {
+      play(event);
+    }
+  };
 
-  // event listener keypress
-  document.addEventListener('keydown', handleKeyPress)
+  // listener to launch event when user presses a key
+  document.addEventListener("keydown", handleKeyPress);
 
   return (
-    <button type="button" id={audioTitle} className="drum-pad" onClick={play}>{assignedKey}</button>
+    <p id={audioTitle} className={`drum-pad ${playing}`} onClick={play}>
+      {assignedKey}
+    </p>
   );
+};
+
+DrumPad.propTypes = {
+  assignedKey: PropTypes.string.isRequired,
+  audioTitle: PropTypes.string.isRequired,
+  audioFile: PropTypes.isRequired,
+  recoverDisplayText: PropTypes.func.isRequired
 };
